f27bfc6a50618bdfe8b0940b04a2a1f078bfc9c9
wwwwiq <wwwwiqfwz@gmail.com>
2014-03-01 08:29:53 +0800

    Create Puzzle.java

---
+++ src/Puzzle.java
@@ -0,0 +1,181 @@
+package rp13.search.interfaces;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class Puzzle {
+	public static String a;
+	public static String c;
+	
+	/**
+	 * Explicit enumeration of moves the blank tile can take.
+	 * 
+	 * @author nah
+	 * 
+	 */
+	
+	public enum PuzzleMove {
+		 LEFT(-1), RIGHT(1), INLEFT(-5), INRIGHT(5);
+
+		private final int m_move;
+		private PuzzleMove(int _move) {
+			m_move = _move;
+		}
+
+
+		/**
+		 * Cached result of values such that copy isn't done every time.
+		 */
+		private static final PuzzleMove[] VALUES = values();
+
+		/***
+		 * Count of values in list
+		 */
+		private static final int SIZE = VALUES.length;
+	}
+	/*
+	public enum IndexMove {
+		INLEFT(-1), INRIGHT(1), INLEFT2(-2), INRIGHT2(2), STAY(0);
+
+		private final int i_move;
+		private IndexMove(int _move) {
+			i_move = _move;
+		}
+		private static final IndexMove[] VALUES = values();
+
+	}
+	*/
+	
+	/**
+	 * The pieces in the puzzle, represented as an array.
+	 */
+	protected final char[] s_board = new char[20];
+	
+	private static char[] c_board;
+	
+	private int m_currentinx = 0;
+
+	/**
+	 * Create an puzzle in its default configuration
+	 * @return 
+	 */
+	private Puzzle() {
+	    a = "rpSearch";
+		int x = a.length();
+		for (int i = 0; i < x; i++) {
+			s_board[i] = a.charAt(i);
+		}
+		
+		
+		
+	}
+	public Puzzle(Puzzle _that) {
+		
+		c_board = Arrays.copyOf(s_board, a.length());
+		m_currentinx = _that.m_currentinx;
+	}
+
+	public boolean equals() {
+
+				return Arrays.equals(s_board, c_board);
+			
+
+
+	}
+	public static Puzzle orderedPuzzle() {
+		return new Puzzle();
+	}
+	public boolean isPossibleMove(PuzzleMove _move) {
+		int newinx = 0;
+		if(_move.m_move == 5)
+		m_currentinx++;
+		else if(_move.m_move == -5)
+			m_currentinx--;
+		else
+		newinx = m_currentinx + _move.m_move;
+			
+		return (newinx >= 0) && (newinx <= a.length()) && (m_currentinx >= 0) && (m_currentinx <= a.length());
+
+
+	}
+
+	public boolean makeMove(PuzzleMove _move) {
+		int newinx = 0;
+		if (isPossibleMove(_move)) {
+			// where should the blank end up
+			if(_move.m_move == 5)
+				m_currentinx++;
+				else if(_move.m_move == -5)
+					m_currentinx--;
+				else
+				{
+			 newinx = m_currentinx + _move.m_move;
+			// get the piece that was in that position
+			char toSwapWith = c_board[newinx];
+			// then swap them around
+//			toSwapWith = c_board[i];
+			c_board[newinx] = c_board[m_currentinx];
+			c_board[m_currentinx] = toSwapWith;
+			m_currentinx = newinx;
+				}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+
+	/**
+	 * Return an puzzle with the pieces in the correct order
+	 * 
+	 * @return Returns an ordered Puzzle.
+	 */
+	public String Answer()
+	{
+		return a;
+	}
+
+	/**
+	 * Creates a randomised eight puzzle using the given number of random moves.
+	 * @return 
+	 */
+	public static void randomPuzzle() {
+		  Random rnd = new Random();
+
+	        int ran; 
+	        char temp;
+		
+		for (int i = 0; i < c_board.length; i++) {
+			
+			ran =  rnd.nextInt(c_board.length);
+			temp = c_board[i];
+			c_board[i] = c_board[ran];
+			c_board[ran] = temp;
+		}
+		c.valueOf(c_board);
+		}
+	
+	
+	private String ctoString() {
+		return c;
+	}
+
+	public static void main(String[] args) {
+		Puzzle p = Puzzle.orderedPuzzle();
+		System.out.println(a);
+
+		
+		for (PuzzleMove move : PuzzleMove.values()) {
+			p.makeMove(move);
+			System.out.println(move);
+			System.out.println(p);
+		}
+
+	}
+
+
+
+
+
+}
